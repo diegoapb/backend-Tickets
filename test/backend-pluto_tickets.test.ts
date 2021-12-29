69125b6cf05399e8375577eeb177456a8538037f
diegoapb <parra.diego@correounivalle.edu.co>
2021-12-29 08:31:04 -0500

    Initial commit

---
+++ test/backend-pluto_tickets.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BackendPlutoTickets from '../lib/backend-pluto_tickets-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/backend-pluto_tickets-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BackendPlutoTickets.BackendPlutoTicketsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
