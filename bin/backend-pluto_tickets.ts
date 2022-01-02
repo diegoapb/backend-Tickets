609624db529db004c035cf20e21fc154cca0bdf4
diegoapb <parra.diego@correounivalle.edu.co>
2022-01-02 15:06:07 -0500

    ADD stacks

--- bin/backend-pluto_tickets.ts
+++ bin/backend-pluto_tickets.ts
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { EventStack } from '../lib/EventStack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
 
+import { EventStack } from "../lib/EventStack";
+import { TicketStack } from "../lib/TicketStack";
+import { ApiStack } from "../lib/ApiStack";
+import { RestApi } from "aws-cdk-lib/aws-apigateway";
 
 const app = new cdk.App();
 
-new EventStack(app, 'EventStack', {
-  stackName: 'EventStack',
+const apiStack = new ApiStack(app, "apiStack",{
+  stackName: "apiStack",
+});
+
+const eventStack = new EventStack(app, "EventStack", {
+  stackName: "EventStack",
+});
+
+const ticketStack = new TicketStack(app, "TicketStack", {
+  stackName: "TicketStack",
 });
